d2a55346528c06caef072e39858530cb5c82c44f
unknown <flecuyer@pc-b10-01.eleves.ecn>
2014-02-18 18:04:46 +0100

    Creation du package model contenant les classes des objets manipules dans l application. Creation de la classe WBSElement representant un element du WBS

---
+++ src/model/WBSElement.java
@@ -0,0 +1,17 @@
+
+package model;
+
+/**
+ * Class representing the model of an element of the WBS
+ * @author flecuyer
+ */
+public class WBSElement {
+    
+    //Attributes
+    
+    /* id of the project this element is related to */
+    protected int project_id;
+    
+    /* label of the element */
+    protected String label;
+}
