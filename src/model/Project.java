06e29d5bc7e87ad27d5fee8271ffb1b903a52a1c
unknown <felix.leo.lecuyer@gmail.com>
2014-02-19 16:39:24 +0100

    creation de la classe project; creation des attributs de la classe WBSElement

---
+++ src/model/Project.java
@@ -0,0 +1,15 @@
+package model;
+
+/**
+ * Class project identifying the current selected project
+ * Singleton class : only one project can be opened at a time
+ * @author Felix
+ */
+public class Project {
+    
+    /* database id of the project */
+    protected long id;
+    
+    /* label of the project */
+    protected String label;
+}
--- src/model/WBSElement.java
+++ src/model/WBSElement.java
@@ -1,6 +1,10 @@
 
 package model;
 
+import java.math.BigDecimal;
+import java.util.GregorianCalendar;
+
+
 /**
  * Class representing the model of an element of the WBS
  * @author flecuyer
@@ -10,8 +14,73 @@ public class WBSElement {
     //Attributes
     
     /* id of the project this element is related to */
-    protected int project_id;
+    protected Project project;
     
     /* label of the element */
-    protected String label;
+    protected String label; 
+    
+    /* description of the element */
+    protected String description;
+    
+    /* boolean indicating if the element is a workpackage */
+    protected boolean isWorkpackage;
+    
+    /* startdate of the element */
+    protected GregorianCalendar startDate;
+    
+    /* workload expected for the element */
+    protected BigDecimal workload;
+    
+    /* expected duration of the element */
+    protected BigDecimal duration;
+    
+    /* boolean indicating if the element is contractual */
+    protected boolean isContractual;
+    
+    /* description of the achievment criterias of the element */
+    protected String achievmentCriteria;
+    
+    /* delivery date of the element */
+    protected GregorianCalendar deliveryDate;
+    
+    /* expected labor amount of the element */
+    protected BigDecimal laborAmount;
+    
+    /* expected amount of purchases for the element */
+    protected BigDecimal purchaseAmount;
+    
+    /* expected amount for expenses */
+    protected BigDecimal expenseAmount;
+    
+    /* expected amount for renting */
+    protected BigDecimal rentAmount;
+    
+    /* amount for subcontractor */
+    protected BigDecimal subcontractAmount;
+    
+    /* early start date of the element */
+    protected GregorianCalendar earlyStart;
+    
+    /* early finish date */
+    protected GregorianCalendar earlyFinish;
+    
+    /* late start date */
+    protected GregorianCalendar lateStart;
+    
+    /* late finish date */
+    protected GregorianCalendar lateFinish;
+    
+    /* total slack of the element */
+    protected BigDecimal totalSlack;
+    
+    /* free slack of the element */
+    protected BigDecimal freeSlack;
+    
+    /* parent element */
+    protected WBSElement parentElement;
+    
+    /* rank of the element on its level */
+    protected int rank;
+   
+    
 }
