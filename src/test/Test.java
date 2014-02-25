78c7c45fcaf152df081ce5ad5cda455c707e0ef2
unknown <felix.leo.lecuyer@gmail.com>
2014-02-25 23:12:32 +0100

    Création d'une classe main de test

---
+++ src/test/Test.java
@@ -0,0 +1,67 @@
+package test;
+
+import dao.impl.ProjectDAO;
+import dao.impl.WBSElementDAO;
+import exceptions.DatabaseException;
+import exceptions.ProjectException;
+import java.math.BigDecimal;
+import java.util.Date;
+import manager.DatabaseManager;
+import model.Project;
+import model.WBSElement;
+
+/**
+ * Test class
+ *
+ * @author Felix
+ */
+public class Test {
+
+    public static void main(String[] args) {
+
+        System.out.println("debut du test");
+
+        try {
+            
+            //Connection à la base de données
+            DatabaseManager dbManager = new DatabaseManager("jdbc:h2:~/test", "sa", "");
+            System.out.println("DatabaseManager créé");
+            
+            //Creation des objets DAO
+            ProjectDAO projetDao = new ProjectDAO(dbManager);
+            WBSElementDAO elementDAO = new WBSElementDAO(dbManager);
+            
+            //Creation d'un nouveau projet
+            Project projetTest = new Project("Projet Test", "Projet Test", "Projet test : test d'insertion et de manipulation d'objets du WBS", "", null);
+            //Insertion du projet dans la base de données
+            Long idProjetTest = projetDao.create(projetTest);
+            //Mise à jour du champ id du projet
+            projetTest.setId(idProjetTest);
+            
+            //Creation d'un nouvel element du WBS
+            Date dateLivraison = new Date(114, 03, 24);
+            WBSElement elementTest = new WBSElement(null, "Element père", "Element de test d'arbre WBS : élément père", false, null, new BigDecimal(10), new BigDecimal(1), true, "Achevé lorsque le projet est achevé", dateLivraison, null, null, null, null, null, null, null, null, null, null, null, null, new Integer(1), projetTest.getId());
+            //Insertion de l'élément dans la base de données
+            Long idElementTest = elementDAO.create(elementTest);
+            
+            //On vérifie que les éléments ont bien été créé dans la base de données
+            WBSElement elementTrouve = elementDAO.find(idElementTest);
+            
+            System.out.println("Element trouvé dans la base :");
+            System.out.println("id : "+elementTrouve.getId());
+            System.out.println("label : "+elementTrouve.getLabel());
+            System.out.println("name : "+elementTrouve.getDescription());
+            System.out.println("isWorkpackage :"+elementTrouve.isIsWorkpackage());
+            System.out.println("Date de livraison :" + elementTrouve.getDeliveryDate());
+            System.out.println("Workload : " + elementTrouve.getWorkload());
+            
+            
+
+        } catch (DatabaseException e) {
+            System.out.println("Erreur BDD :" + e.getMessage() + ", ResultCode :" + e.getResultCode().name());
+        }
+        catch(ProjectException e){
+            System.out.println("Erreur Projet :" + e.getMessage() + ", ResultCode :" + e.getResultCode().name());
+        }
+    }
+}
